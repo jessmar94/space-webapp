e8bbcb2dbcb1cb33b1cd927d0dab37d2d0fc2c4a
Jessica Marais <jessica_marais@hotmail.com>
2020-01-08 15:50:37 +0000

    Passing test for datetime method. Need to mock API

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Space Webapp
 
-Using open API from [Open Notify](http://open-notify.org/). 
+Using open API from [Open Notify](http://open-notify.org/).
+
+## How to Run Tests
+```bash
+python -m unittest tests/test_api.py
+```
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,6 @@
 import unittest
 from model.api import API
+import json, requests
 
 class TestAPI(unittest.TestCase):
 
@@ -9,7 +10,53 @@ class TestAPI(unittest.TestCase):
     def test(self):
         self.assertTrue(True)
 
-
+    def test_datetime(self):
+        response2 = {
+            "message": "success",
+            "request": {
+                "altitude": 100,
+                "datetime": 1578497662,
+                "latitude": 40.71,
+                "longitude": -74.0,
+                "passes": 5
+            },
+            "response": [
+                {
+                    "duration": 466,
+                    "risetime": 1578537826
+                },
+                {
+                    "duration": 650,
+                    "risetime": 1578543504
+                },
+                {
+                    "duration": 609,
+                    "risetime": 1578549348
+                },
+                {
+                    "duration": 556,
+                    "risetime": 1578555238
+                },
+                {
+                    "duration": 605,
+                    "risetime": 1578561078
+                }
+            ]
+        }
+        parameters = {
+            "lat": 40.71,
+            "lon": -74
+        }
+        response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
+        results = [
+            'Thu 09 Jan 2020, 02:43',
+            'Thu 09 Jan 2020, 04:18',
+            'Thu 09 Jan 2020, 05:55',
+            'Thu 09 Jan 2020, 07:33',
+            'Thu 09 Jan 2020, 09:11'
+            ]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.api.datetime(response), results)
 
 if __name__ == '__main__':
     unittest.main()
