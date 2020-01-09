c1cf15fe9506a033e2ca9f8cbe8fb6b4fc893b84
Jessica Marais <jessica_marais@hotmail.com>
2020-01-09 16:04:58 +0000

    Site now working for all cities in drop-down

--- model/lat_long.py
+++ model/lat_long.py
@@ -3,11 +3,26 @@ class Coordinates:
     def lat_long(city):
         params = {
             "lat": 0,
-            "lon": 0,
+            "lon": 0
         }
         if city == "New York":
             params.update({
                 "lat": 40.71,
-                "lon": -74,
+                "lon": -74
             })
-        return params 
+        if city == "London":
+            params.update({
+                "lat": 51.50853,
+                "lon": -0.12574
+            })
+        if city == "Cape Town":
+            params.update({
+                "lat": -33.9188610,
+                "lon": 18.42330
+            })
+        if city == "Tokyo":
+            params.update({
+                "lat": 35.652832,
+                "lon": 139.839478
+            })
+        return params
